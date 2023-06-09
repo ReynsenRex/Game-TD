1c4380546ace417488ef17e7e1c691c19a519464
Kenn <kenncahyaputra142@gmail.com>
2023-06-09 16:21:53 +0700

    tower edit

--- core/src/com/mygdx/game/Tower.java
+++ core/src/com/mygdx/game/Tower.java
@@ -1,4 +1,9 @@
 package com.mygdx.game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Tower {
+
+    private int atkSpeed;
+    private int damage;
 }
