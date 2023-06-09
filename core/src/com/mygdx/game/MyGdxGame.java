88e355b46a4df21b53f7906960737925431ba86d
ReynsenRex <reynsenr@gmail.com>
2023-06-09 16:32:37 +0700

    Zombie animation

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -11,7 +11,6 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
-//COBA
 public class MyGdxGame extends ApplicationAdapter {
     Animation<TextureRegion> zombieAnimation;
     private Texture texture, zombieTexture;
--- core/src/com/mygdx/game/Zombie.java
+++ core/src/com/mygdx/game/Zombie.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Zombie {
+    private float stateTime; // Animation time for the zombie
     public Texture texture;
     public Sprite sprite;
     public float x;
@@ -11,11 +12,20 @@ public class Zombie {
     public float targetX;
     public float targetY;
     public boolean moving;
-
     public int health;
 
     public Zombie() {
         health = 100; // Set default health to 100
+    }
+
+    // Other methods and constructors
+
+    public float getStateTime() {
+        return stateTime;
+    }
+
+    public void setStateTime(float stateTime) {
+        this.stateTime = stateTime;
     }
 
 }
